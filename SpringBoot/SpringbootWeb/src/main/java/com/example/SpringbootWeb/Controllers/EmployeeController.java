89c0a7fa45f7b08d9ac8b737bd025e2051633443
sushant <bksushantofficial@gmail.com>
2026-04-04 13:23:44 +0545

    Learn Spring Boot presentation layer with REST endpoints

--- SpringBoot/SpringbootWeb/src/main/java/com/example/SpringbootWeb/Controllers/EmployeeController.java
+++ SpringBoot/SpringbootWeb/src/main/java/com/example/SpringbootWeb/Controllers/EmployeeController.java
@@ -1,4 +1,64 @@
 package com.example.SpringbootWeb.Controllers;
 
+
+import com.example.SpringbootWeb.dto.EmployeeDTO;
+import org.springframework.web.bind.annotation.*;
+
+
+//@Controller is used to mark a class as a Spring MVC controller.
+//@RestController is a specialized version of @Controller.
+//        What it does:
+//        Combines:
+//        @Controller
+//        @ResponseBody
+//        Tells Spring: “Return data directly as HTTP response (JSON/XML), not a view.”
+
+@RestController
+@RequestMapping("/employees") // Now this is the parent path like the and those below are now the the children path like employees/employeeID
+
+
 public class EmployeeController {
+
+
+//    @GetMapping("/message")
+//    public String message(){
+//        return "my first spring get request";
+//    }
+
+
+//
+//    What is @PathVariable?
+//    Short answer: It lets you grab values from the URL path and use them as method parameters.
+
+    @GetMapping("/{employeeID}")
+    public EmployeeDTO getEmployeeById(@PathVariable(name="employeeID") long id){
+        return new EmployeeDTO(id,"miku","miku123","miku@gmail.com");
+    }
+
+//    @Requestparam example
+//Short answer: It grabs values from the Query String (the part of the URL after the ?) and injects them into your method.
+//    Your Example (Expanded):
+//    Imagine you want to search for employees by department or sort them.
+
+
+    @GetMapping // here this is the default path because im not appending anything
+    public String getEmployeeByAge(@RequestParam(required = false) Integer age, // now passing this is optional its upto you if you wanna pass or not
+                                   @RequestParam(required = false) String sortBy){
+        return "Hi your age is " + age +" " + sortBy;
+    }
+
+    @PostMapping //Post is used whenever you want to create
+    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+        inputEmployee.setUsername("nezuko");
+        inputEmployee.setId(20L);
+        return inputEmployee;
+    }
+
+    @PutMapping
+    public String updateEmployee(){
+        return "hello from put";
+    }
+
+
+
 }
---
+++ SpringBoot/SpringbootWeb/src/main/java/com/example/SpringbootWeb/dto/EmployeeDTO.java
@@ -0,0 +1,57 @@
+package com.example.SpringbootWeb.dto;
+
+
+//DTO = Data Transfer Object
+//A simple object used to transfer data between layers (like from controller → service → client).
+//It does not contain business logic. Its only job is to carry data.
+//Think of it as an envelope: it just carries information; it doesn’t do anything with it.
+public class EmployeeDTO {
+     Long id;
+     String username;
+     String password; // sensitive
+     String email;
+
+     public EmployeeDTO(){
+
+     }
+
+    public EmployeeDTO(Long id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
